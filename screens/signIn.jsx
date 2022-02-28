fbb90cfe16fc30513af492c5750b9244659278ce
RVolat <93164797+RVolat@users.noreply.github.com>
2022-02-28 15:32:18 +0100

    sing in / up style done

--- screens/signIn.jsx
+++ screens/signIn.jsx
@@ -1,12 +1,134 @@
+//////////////////////////////////////IMPORT///////////////////////////////////////////////
 import React from "react";
-import { View } from "react-native"
-import { Button, Input } from "react-native-elements";
+import { View, Image, StyleSheet } from "react-native";
+import { Button, Input, Text } from "react-native-elements";
+import { Ionicons } from "@expo/vector-icons";
 
-function signIn() {
-  return <View>
-      <Button title= "test"/>
+///////////////////////////////////Function//////////////////////////////////////////////////
+function signIn(props) {
+  return (
+    <View style={styles.background}>
+      <View style={styles.header}>
+        <Ionicons
+          name="close"
+          size={25}
+          color="#BDBDBD"
+          onPress={() => props.navigation.navigate("BottomNavigator")}
+        />
 
-  </View>;
+        <Text style={styles.inscription}>Connexion</Text>
+        <Text
+          onPress={() => props.navigation.navigate("SignIn")}
+          style={styles.connexion}
+        >
+          Inscription
+        </Text>
+      </View>
+      <View style={styles.logo}>
+        <Image
+          style={styles.image}
+          source={require("../assets/logo-gachaBook.png")}
+        />
+        <Text style={styles.text}>GachaBook</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Input
+          containerStyle={{ width: 370 }}
+          inputStyle={styles.input}
+          placeholder="Email"
+        />
+        <Input
+          containerStyle={{ width: 370 }}
+          inputStyle={styles.input}
+          placeholder="Mot de passe"
+          secureTextEntry={true}
+        />
+        <Button
+          buttonStyle={styles.facebook}
+          title={"Connexion avec Facebook"}
+          icon={
+            <Ionicons
+              name="logo-facebook"
+              size={24}
+              color="white"
+              style={{ marginRight: 20 }}
+            />
+          }
+        />
+        <Button
+          buttonStyle={styles.google}
+          title="Connexion avec Google    "
+          icon={
+            <Ionicons
+              name="logo-google"
+              size={24}
+              color="white"
+              style={{ marginRight: 20 }}
+            />
+          }
+        />
+        <Button buttonStyle={styles.signUp} title="Connexion" />
+        <Text style={styles.connexion}>Mot de passe oublier ? </Text>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    backgroundColor: "#1E202A",
+    justifyContent: "space-evenly",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  logo: {
+    alignItems: "center",
+  },
+  image: {
+    width: 100,
+    height: 100,
+  },
+  text: {
+    color: "white",
+    fontSize: 35,
+  },
+  inscription: {
+    color: "white",
+    fontSize: 35,
+    marginLeft: 35,
+  },
+  buttonContainer: {
+    alignItems: "center",
+  },
+  facebook: {
+    borderRadius: 50,
+    width: 300,
+    marginBottom: 10,
+  },
+  google: {
+    backgroundColor: "red",
+    borderRadius: 50,
+    width: 300,
+    marginBottom: 10,
+  },
+  signUp: {
+    backgroundColor: "#F5960D",
+    borderRadius: 50,
+    width: 300,
+    marginBottom: 10,
+  },
+  connexion: {
+    color: "#F5960D",
+  },
+  input: {
+    borderRadius: 5,
+    backgroundColor: "white",
+  },
+});
+///////////////////////////////////Styles//////////////////////////////////////////////////
 
 export default signIn;
--- screens/signUp.jsx
+++ screens/signUp.jsx
@@ -2,18 +2,27 @@
 import React from "react";
 import { View, Image, StyleSheet } from "react-native";
 import { Button, Input, Text } from "react-native-elements";
+import { Ionicons } from "@expo/vector-icons";
 
 ///////////////////////////////////Function//////////////////////////////////////////////////
 function signUp(props) {
   return (
     <View style={styles.background}>
       <View style={styles.header}>
-        <Button
-          title="X"
+        <Ionicons
+          name="close"
+          size={25}
+          color="#BDBDBD"
           onPress={() => props.navigation.navigate("BottomNavigator")}
         />
-        <Text style={styles.text}>Inscription</Text>
-        <Button title="Connexion" />
+
+        <Text style={styles.inscription}>Inscription</Text>
+        <Text
+          onPress={() => props.navigation.navigate("SignIn")}
+          style={styles.connexion}
+        >
+          Connexion
+        </Text>
       </View>
       <View style={styles.logo}>
         <Image
@@ -22,13 +31,48 @@ function signUp(props) {
         />
         <Text style={styles.text}>GachaBook</Text>
       </View>
-      <View>
-        <Input placeholder="Nom" />
-        <Input placeholder="Email" />
-        <Input placeholder="Mot de passe" />
-        <Button buttonStyle={styles.buttton} title="Inscription avec Facebook" />
-        <Button title="Inscription avec Google" />
-        <Button title="Inscription" />
+      <View style={styles.buttonContainer}>
+        <Input
+          containerStyle={{ width: 370 }}
+          inputStyle={styles.input}
+          placeholder="Nom"
+        />
+        <Input
+          containerStyle={{ width: 370 }}
+          inputStyle={styles.input}
+          placeholder="Email"
+        />
+        <Input
+          containerStyle={{ width: 370 }}
+          inputStyle={styles.input}
+          placeholder="Mot de passe"
+          secureTextEntry={true}
+        />
+        <Button
+          buttonStyle={styles.facebook}
+          title={"Inscription avec Facebook"}
+          icon={
+            <Ionicons
+              name="logo-facebook"
+              size={24}
+              color="white"
+              style={{ marginRight: 20 }}
+            />
+          }
+        />
+        <Button
+          buttonStyle={styles.google}
+          title="Inscription avec Google    "
+          icon={
+            <Ionicons
+              name="logo-google"
+              size={24}
+              color="white"
+              style={{ marginRight: 20 }}
+            />
+          }
+        />
+        <Button buttonStyle={styles.signUp} title="Inscription" />
       </View>
     </View>
   );
@@ -43,6 +87,7 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
   },
   logo: {
     alignItems: "center",
@@ -55,9 +100,37 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 35,
   },
-  buttton: {
+  inscription: {
+    color: "white",
+    fontSize: 35,
+    marginLeft: 35,
+  },
+  buttonContainer: {
+    alignItems: "center",
+  },
+  facebook: {
     borderRadius: 50,
-    
+    width: 300,
+    marginBottom: 10,
+  },
+  google: {
+    backgroundColor: "red",
+    borderRadius: 50,
+    width: 300,
+    marginBottom: 10,
+  },
+  signUp: {
+    backgroundColor: "#F5960D",
+    borderRadius: 50,
+    width: 300,
+    marginBottom: 10,
+  },
+  connexion: {
+    color: "#F5960D",
+  },
+  input: {
+    borderRadius: 5,
+    backgroundColor: "white",
   },
 });
 
